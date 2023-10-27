17eefb0da13c21c9eb782f5cfe93a3ffd1d5e04c
dam1 <dam1@Aulainformatica32>
2023-10-27 08:52:36 +0200

    Terminado boletin

---
+++ src/diasemana/DiaSemana.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package diasemana;
+
+/**
+ *
+ * @author dam1
+ */
+public class DiaSemana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        public void verLetra(int dia);
+        else if(n1==1) {
+    System.out.println("Luns");
+        }
+        
+        else if (n1==2){
+    System.out.println("Martes");
+}
+        else if (n1==3){
+    System.out.println("Mercores");
+}
+        else if (n1==4){
+    System.out.println("Xoves");
+}
+        else if (n1==5){
+    System.out.println("Venres");
+}
+        else if(n1==6){
+    System.out.println("Sabado");    
+}
+        else if(n1==7){
+    System.out.println("Domingo");
+    
+}
+    }
+    
+}
